b836f2a314a0204bf274dd1b05143a4feeeb14f5
Rock_Crowler <belmaspi@yandex.ru>
2019-08-19 17:58:10 +0300

    Start Algorithms & data Structure Project

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// первый коммит
+        System.out.println("Hellow World!!!");
+        System.out.println("Hellow Progers!!!");
+        System.out.println("Hellow Progers!!!");
+
+
+    }
+}
