6838047717bf4c4a61d1b27bc0c39e7ad37196ce
Rock_Crowler <belmaspi@yandex.ru>
2019-08-22 20:44:37 +0300

    HomeWork ex3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,47 @@
 package com.company;
 
+import java.util.Collection;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// первый коммит
-        System.out.println("Hellow World!!!");
-        System.out.println("Hellow Progers!!!");
-        System.out.println("Hellow Progers!!!");
+
+        String tmp;
+        Scanner sc = new Scanner(System.in);
+
+        while (true){
+            System.out.println("Введите строку символов: ");
+            tmp = sc.nextLine();
+
+           if (tmp.length()!=0) break;
+        }
+
+        Dequeue deq = new Dequeue(tmp.length());
+
+        for (int i = 0; i < tmp.length(); i++) {
+            deq.pushFront(tmp.charAt(i));
+        }
+
+        System.out.println("Перевернутая строка:  ");
+        deq.print();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
